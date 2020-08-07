32484c0db477c4834accfd977cec9644834dd764
Arjun kushwah <53173361+kushwaharjun@users.noreply.github.com>
2020-08-07 15:05:26 +0530

    Add files via upload
    initial commit

---
+++ src/main/java/io/inflix/moviinfoservice/MoviInfoServiceApplication.java
@@ -0,0 +1,13 @@
+package io.inflix.moviinfoservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviInfoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviInfoServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/inflix/moviinfoservice/MoviInfoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.inflix.moviinfoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviInfoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
